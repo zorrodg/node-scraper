c4f3f0e2f3d0dd88c9ab41404d196e63907588ad
Andrés Zorro <zorrodg@gmail.com>
2014-11-16 13:39:30 -0500

    Construyendo URLs

--- functions/list.js
+++ functions/list.js
@@ -1,15 +1,26 @@
 var fs = require('fs'),
-  purify = require('./purify');
+  purify = require('./purify'),
+  diacritic = require('diacritic');
 
 module.exports = function($, url){
-  var todas = $('#cssmenu > ul > li'), seccion, rutas, ruta;
+  var todas = $('#cssmenu > ul > li'), 
+    seccion, rutas, ruta, urlRuta, arrayFinal = [];
 
   for(var i = 0, len = todas.length; i < len; i++){
     seccion = $(todas[i]);
     rutas = seccion.find('ul > li');
     for(var j = 0, len2 = rutas.length; j < len2; j++){
-      ruta = $(rutas[j]);
-      console.log(purify(ruta.html()));
+      ruta = purify($(rutas[j]).html());
+      urlRuta = ruta
+        .toLowerCase()
+        .replace(/[>.:]/g, '')
+        .replace(' - ', ' ')
+        .replace(' – ', '')
+        .replace(/\s/g, '_');
+      urlRuta = 'http://www.sitp.gov.co/publicaciones/' + diacritic.clean(urlRuta) + '_pub';
+      arrayFinal.push(urlRuta);
     }
   }
+
+  console.log(arrayFinal);
 }
